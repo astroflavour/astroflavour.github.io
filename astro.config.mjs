1f36a6a0dea2e748f27d46e993c6b721fd0e49e6
Lukasz Adam <lukasz.adms@gmail.com>
2023-04-06 11:59:46 +0200

    yes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,6 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from 'astro/config';
 
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://astroflavour.github.io',
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    mdx(),
-    sitemap(),
-  ],
+    site: 'https://astroflavour.github.io',
 });
